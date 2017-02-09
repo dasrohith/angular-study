b4456643b189959cd87f1e181fcefd13c919dd5b
Rohith Thulasidas <dasrohith@gmail.com>
2017-02-09 09:28:56 +0530

    Increased the number of products and handled UI using ng-repeat

--- angular-web/src/main/webapp/js/app.js
+++ angular-web/src/main/webapp/js/app.js
@@ -2,13 +2,15 @@
  * 
  */
 (function(){
-	var gem = {name:'Azurite', price: 2.2};
-
+	var gems = [ 
+		{ name:'Azurite', price: 1.2, description:'This is a description for Azurite Gem', canPurchase: false },
+		{ name:'Ruby', price: 2.2, description:'This is a description for Ruby Gem', canPurchase: true }
+		];
 	var app = angular.module('gemStore',[]);
 	
 	
 	app.controller('StoreController',function(){
-		this.product = gem;
+		this.products = gems;
 	});
 	
 })();
