a78e0f0eac223ee37e7bde2ed5a746dc48d96cf2
Rohith Thulasidas <dasrohith@gmail.com>
2017-02-09 07:15:16 +0530

    Controller Added to the Application

--- angular-web/src/main/webapp/js/app.js
+++ angular-web/src/main/webapp/js/app.js
@@ -1,4 +1,14 @@
 /**
  * 
  */
-var app = angular.module('gemStore',[]);
+(function(){
+	var gem = {name:'Azurite', price: 2.2};
+
+	var app = angular.module('gemStore',[]);
+	
+	
+	app.controller('StoreController',function(){
+		this.product = gem;
+	});
+	
+})();
